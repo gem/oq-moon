8fe86a1ce59902566787b1720c0a61cf9fb95245
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-08-22 15:00:21 +0200

    added sleep before maximize window

--- openquake/moon/base.py
+++ openquake/moon/base.py
@@ -82,6 +82,7 @@ class Moon(object):
 
         # screencast: comment maximize_window() line
         # if you want to set a specific window size
+        time.sleep(2)
         self.driver.maximize_window()
         self.main_window = None
 
