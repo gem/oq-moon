08a5db0373dcc890ecf2946d3ef8035d0ca3b729
Matteo Nastasi <nastasi@openquake.org>
2017-11-09 16:35:50 +0100

    increased bugfix version

--- openquake/moon/__init__.py
+++ openquake/moon/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = ['FailureCatcher', 'Moon', 'TimeoutError', 'NotUniqError']
 from base import Moon
 from utils import TimeoutError, NotUniqError
