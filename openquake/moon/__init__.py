f7bb8d94fb70ff799f524f1a30fd20b9550062b8
Matteo Nastasi <nastasi@openquake.org>
2017-11-13 10:44:16 +0100

    proper way to manage relative imports (python 2 and 3)

--- openquake/moon/__init__.py
+++ openquake/moon/__init__.py
@@ -15,9 +15,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
-
+from .base import Moon
+from .utils import TimeoutError, NotUniqError
+from .failurecatcher import FailureCatcher
 __version__ = "0.3.0"
 __all__ = ['FailureCatcher', 'Moon', 'TimeoutError', 'NotUniqError']
-from openquake.moon.base import Moon
-from openquake.moon.utils import TimeoutError, NotUniqError
-from openquake.moon.failurecatcher import FailureCatcher
--- openquake/moon/base.py
+++ openquake/moon/base.py
@@ -18,7 +18,7 @@
 
 import time
 import sys, os
-from openquake.moon.utils import TimeoutError, NotUniqError, wait_for
+from .utils import TimeoutError, NotUniqError, wait_for
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
