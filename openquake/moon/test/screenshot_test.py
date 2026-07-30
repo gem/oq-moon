111f1eca4f23a91df2ebcfeadcb098a7d779ca9a
Matteo Nastasi <nastasi@openquake.org>
2026-07-30 15:55:30 +0200

    disable screenshot test

--- openquake/moon/test/screenshot_test.py
+++ openquake/moon/test/screenshot_test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import unittest
-from nose.plugins.attrib import attr
+import pytest
+# from nose.plugins.attrib import attr
 from openquake.moon.test import pla
 
+@pytest.mark.skip(reason="disable screenshots during first phase of nose-pytest migration")
 class ScreenshotTest(unittest.TestCase):
-    @attr('negate')
+#    @attr('negate')
     def screenshot_test(self):
         pla.get('/screenshot_test.html')
 
