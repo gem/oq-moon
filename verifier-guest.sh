0b7aaa5ef7c18c2b7acc4bcf08cdb7af267208ce
Matteo Nastasi <nastasi@openquake.org>
2026-07-30 11:42:52 +0200

    typo fixed

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -29,7 +29,7 @@ exec_test () {
     wget "http://ftp.openquake.org/mirror/mozilla/geckodriver-v${GEM_GECKODRIVER_VERSION}-linux64.tar.gz"
     tar zxvf "geckodriver-v${GEM_GECKODRIVER_VERSION}-linux64.tar.gz"
     sudo cp geckodriver /usr/local/bin
-    sudo pip install -U selenium==${GEM_SELENIUM_VERSION}
+    pip install -U selenium==${GEM_SELENIUM_VERSION}
 
     cp $GEM_GIT_PACKAGE/openquake/moon/test/config/moon_config.py.tmpl $GEM_GIT_PACKAGE/openquake/moon/test/config/moon_config.py
     export DISPLAY=:1
