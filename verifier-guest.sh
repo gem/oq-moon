92494dd10163029374a4b3d10be1417613789b36
Matteo Nastasi <nastasi@openquake.org>
2026-07-30 16:19:40 +0200

    disabled screenshot test

--- verifier-guest.sh
+++ verifier-guest.sh
@@ -39,10 +39,13 @@ exec_test () {
     err=0
     # python -m openquake.moon.nose_runner --failurecatcher dev -s -v -a '!negate' --with-xunit --xunit-file=xunit-moon-dev.xml $GEM_GIT_PACKAGE/openquake/moon/test || err=1
     pytest --tb=short -vs $GEM_GIT_PACKAGE/openquake/moon/test || err=1
-    for negate_file in screenshot_test; do
+    # for negate_file in screenshot_test; do
+    # FIXME temporarily disable as first step of nose to pytest migration
+    for negate_file in; do
         beg_date="$(date "+%d/%b/%Y %H:%M:%S")"
         time_begin="$(date +%s%N)"
-        python -m openquake.moon.nose_runner --failurecatcher dev-neg -s -v -a 'negate' --with-xunit --xunit-file=xunit-moon-dev-neg.xml "$GEM_GIT_PACKAGE/openquake/moon/test/${negate_file}.py" || true
+        # python -m openquake.moon.nose_runner --failurecatcher dev-neg -s -v -a 'negate' --with-xunit --xunit-file=xunit-moon-dev-neg.xml "$GEM_GIT_PACKAGE/openquake/moon/test/${negate_file}.py" || true
+        pytest --tb=short -vs $GEM_GIT_PACKAGE/openquake/moon/test || err=1
         time_end="$(date +%s%N)"
         float_sec_time="$(echo "($time_end - $time_begin) / 1000000000" | bc -l | sed 's/\.\([0-9]\{3\}\).*$/.\1/g')"
         if [ ! -f  dev-neg_openquake.moon.test.*.${negate_file}.png ]; then
